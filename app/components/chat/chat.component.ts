6e3d7013710386832ba9709a3a226524791155e9
nahwinrajan <nahwin.rajan@gmail.com>
2016-11-03 13:08:37 +0700

    finish adding user

--- app/components/chat/chat.component.ts
+++ app/components/chat/chat.component.ts
@@ -11,13 +11,15 @@ export class ChatComponent implements OnInit, OnDestroy {
   messages:any=[];
   message:string;
   connection: any;
+  username:string;
+  alert:any = false;
 
   constructor(private _chatService:ChatService) {
 
   }
 
   sendMessage(message:string){
-    this._chatService.sendMessage(this.message);
+    this._chatService.sendMessage(this.message, this.username);
     this.message = '';
   }
 
@@ -29,5 +31,10 @@ export class ChatComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.connection.unsubscribe();
+  }
+
+  addUsername() {
+    this._chatService.setUsername(this.username);
+    this.alert = 'Username set';
   }
 }
--- app/services/chat.service.ts
+++ app/services/chat.service.ts
@@ -7,8 +7,8 @@ export class ChatService {
   private url = 'http://localhost:8000';
   private socket:any;
 
-  sendMessage(message:string) {
-    this.socket.emit('add-message', message);
+  sendMessage(message:string, username:string) {
+    this.socket.emit('add-message', message, username);
   }
 
   getMessages() {
@@ -22,5 +22,14 @@ export class ChatService {
       }
     })
     return observable;
+  }
+
+  getUsername() {
+    return sessionStorage.getItem('username');
+  }
+
+  setUsername(username:string) {
+    console.log('Username set: ' + username);
+    sessionStorage.setItem('username', username);
   }
 }
