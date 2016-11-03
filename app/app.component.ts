28c70bc50b181ae450020ab86394fa3096775fd0
nahwinrajan <nahwin.rajan@gmail.com>
2016-11-03 12:16:15 +0700

    finish chat component + service structure

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,10 @@
+//decorator / angular package / module
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: `<h1>My First Angular App</h1>`
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: 'app.component.html'
 })
+
 export class AppComponent { }
---
+++ app/services/chat.service.ts
@@ -0,0 +1,11 @@
+import { Injectable }     from '@angular/core';
+import { Subject }        from 'rxjs/Subject';
+import { Observable }     from 'rxjs/Observable';
+import * as io            from 'socket.io-client';
+
+export class ChatService {
+  private url = 'http://localhost:8000';
+  private socket:any;
+
+
+}
